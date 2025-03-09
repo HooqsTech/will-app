3086041dfea0904d47ce4ae14fdfe1be62d7eab5
Paranthaman <paranthaman.venkatesan@bright-river.com>
2025-03-09 14:34:43 +0530

    Login page

---
+++ will-app-frontend/src/components/CustomSnackBar.tsx
@@ -0,0 +1,27 @@
+import { Alert, Snackbar } from '@mui/material';
+
+export type TAlertType = 'error' | 'warning' | 'info' | 'success';
+
+interface ICustomSnackBarPros {
+    alertType: TAlertType ;
+    message: string;
+    open: boolean;
+    onClose: () => void;
+}
+const CustomSnackBar:React.FC<ICustomSnackBarPros> = ({alertType,message,open,onClose}) => {
+
+  return (
+    <Snackbar
+        open={open}
+        autoHideDuration={3000} // Hides after 3 seconds
+        onClose={onClose}
+        anchorOrigin={{ "vertical": "top", "horizontal":"right" }}
+      >
+        <Alert onClose={onClose} severity={alertType}>
+          {message}
+        </Alert>
+      </Snackbar>
+  );
+};
+
+export default CustomSnackBar;
