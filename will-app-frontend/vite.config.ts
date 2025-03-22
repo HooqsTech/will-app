b3b1239b4f4d9b1835ceef11fd6a89ede2b1b4ac
Dhanush Ragav <dhanush.ragav@bright-river.com>
2025-03-22 10:56:26 +0530

    pushed the changes..

--- will-app-frontend/vite.config.ts
+++ will-app-frontend/vite.config.ts
@@ -5,9 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 //https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
-server: {
-  host: '127.0.0.1', // Set host to 127.0.0.1
-  port: 3000,        // Set port to 3000
-},
+  base: "/"
+// server: {
+//   host: '127.0.0.1', // Set host to 127.0.0.1
+//   port: 3000,        // Set port to 3000
+// },
 })
