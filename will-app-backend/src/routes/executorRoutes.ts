1d3ae0ec01c7f015f019f6005b9b034fd563c91d
Paranthaman <paranthaman.venkatesan@bright-river.com>
2025-03-25 11:54:55 +0530

    update executor routes

--- will-app-backend/src/routes/executorRoutes.ts
+++ will-app-backend/src/routes/executorRoutes.ts
@@ -1,18 +1,18 @@
 import express from 'express';
 import {
-    getBeneficiaryById,
-    getBeneficiariesByUserId,
-    upsertBeneficiary,
-    deleteBeneficiaryById,
-    deleteBeneficiariesByUserId
-} from '../controller/beneficiariesController';
+    getExecutorById,
+    getExecutorsByUserId,
+    upsertExecutor,
+    deleteExecutorById,
+    deleteExecutorsByUserId
+} from '../controller/executorController';
 
 const router = express.Router();
 
-router.get('/executor/getByUserId', getBeneficiariesByUserId); 
-router.get('/executor/getById', getBeneficiaryById); 
-router.post('/executor/upsert', upsertBeneficiary); 
-router.delete('/executor/deleteById', deleteBeneficiaryById); 
-router.delete('/executor/deleteByUserId', deleteBeneficiariesByUserId); 
+router.get('/executor/getByUserId', getExecutorsByUserId); 
+router.get('/executor/getById', getExecutorById); 
+router.post('/executor/upsert', upsertExecutor); 
+router.delete('/executor/deleteById', deleteExecutorById); 
+router.delete('/executor/deleteByUserId', deleteExecutorsByUserId); 
 
 export default router;
