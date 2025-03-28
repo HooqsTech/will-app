a97920f8be7fcecef2eea8eae6e2dd33707a59ac
Sabari Manikandan <sabari.manikandan@bright-river.com>
2025-03-28 12:20:41 +0530

    added light green as background

--- will-app-frontend/src/pages/YourWill.tsx
+++ will-app-frontend/src/pages/YourWill.tsx
@@ -439,7 +439,7 @@ const YourWill: React.FC = () => {
                         <Sidebar2 />
                     </div>
                 </div>
-                <div className='h-full px-2.5 overflow-hidden flex col-span-12 md:col-span-9 items-center md:p-10 justify-center'>
+                <div className='h-full bg-[#ccdcdc] px-2.5 overflow-hidden flex col-span-12 md:col-span-9 items-center md:p-10 justify-center'>
                     <Routes>
                         <Route path={ROUTE_PATHS.ABOUT_YOU} element={<AboutYouPage />} />
                         <Route path={ROUTE_PATHS.PERSONAL_DETAILS} element={<PersonalDetailsPage />} />
