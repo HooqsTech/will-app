92055096ba3ee156ef1f9334f65bb938d6daca56
Sabari Manikandan <sabari.manikandan@bright-river.com>
2025-03-30 10:50:19 +0530

    added home icon to header in mobile view

--- will-app-frontend/src/components/Header.tsx
+++ will-app-frontend/src/components/Header.tsx
@@ -5,6 +5,8 @@ import Sidebar2 from "./Sidebar2";
 import { Drawer } from "@mui/material";
 import { useRecoilState } from "recoil";
 import { drawerState } from "../atoms/drawerState";
+import { BiHome } from "react-icons/bi";
+import { FaHome } from "react-icons/fa";
 
 const Header = () => {
     const navigate = useNavigate();
@@ -24,12 +26,18 @@ const Header = () => {
                     <img src="/assets/hamara-logo-icon.png" className="h-10" alt="Flowbite Logo" />
                     <span className="self-center text-2xl text-will-green whitespace-nowrap uppercase">Hamara Will</span>
                 </a>
-                <button onClick={() => setIsDrawerOpen(true)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
+                <div className="flex items-center text-gray-500 space-x-3">
+                    <a href="/home">
+                        <BiHome className="text-2xl" />
+                    </a>
+                    <button onClick={() => setIsDrawerOpen(true)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 items-center rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                         <li>
